858d8d962fa57ace48fd00c1e97b1ea247012cfd
leoassuncao <leo.assuncao92@gmail.com>
2018-12-04 14:37:34 -0200

    autoscroll chat

--- src/app/components/message-list/message-list.component.ts
+++ src/app/components/message-list/message-list.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import {Message} from '../../models';
+import { MessageItemComponent } from '../../components/message-item/message-item.component';
 
 @Component({
   selector: 'message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnInit, AfterViewInit {
 
   @Input('messages')
-  private messages : Message[];
+  private messages: Message[];
+
+  @ViewChild('chatlist', { read: ElementRef }) chatList: ElementRef;
+  @ViewChildren(MessageItemComponent, { read: ElementRef }) chatItems: QueryList<MessageItemComponent>;
 
   constructor() { }
+
+  ngAfterViewInit() {
+    this.chatItems.changes.subscribe(elements => {
+      this.scrollToBottom();
+    });
+  }
+
+  private scrollToBottom(): void {
+    try {
+      this.chatList.nativeElement.scrollTop = this.chatList.nativeElement.scrollHeight;
+    }
+    catch (err) {
+      console.log('Could not find the "chatList" element.');
+    }
+  }
 
   ngOnInit() {
   }
